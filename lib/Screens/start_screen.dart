f7dd70cc1e2c91a4e9a382aab63d0af30a87a2c8
MahirAhmadEbrahimi <mahirahmadebrahimi@gmail.com>
2026-04-12 03:36:46 -0700

    now image is working in jewelry project and stater Screen is compleated .

--- lib/Screens/start_screen.dart
+++ lib/Screens/start_screen.dart
@@ -1,1 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
 
+class StartScreen extends StatefulWidget {
+  const StartScreen({super.key});
+
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // wait 3 seconds
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/images/logo.png",
+          opacity: const AlwaysStoppedAnimation(0.9),
+        ),
+      ),
+    );
+  }
+}
