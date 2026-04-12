ca49292009dfa6568ea3e6fcc98fd61d2de839e8
MahirAhmadEbrahimi <mahirahmadebrahimi@gmail.com>
2026-04-12 04:02:14 -0700

    adding navigation bar to project

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -1,3 +1,35 @@
+// import 'package:flutter/material.dart';
+
+// class MainScreen extends StatelessWidget {
+//   const MainScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Jewelry", style: TextStyle(color: Colors.green)),
+//       ),
+//       body: Center(child: Text("Main Screen", style: TextStyle(fontSize: 24))),
+
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const [
+//           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.category),
+//             label: "Categories",
+//           ),
+//           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+//           BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Contact"),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.favorite),
+//             label: "Favorites",
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -5,8 +37,41 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Main Screen", style: TextStyle(fontSize: 24))),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Jewelry", style: TextStyle(color: Colors.green)),
+      ),
+
+      body: const Center(
+        child: Text("Main Screen", style: TextStyle(fontSize: 24)),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 2, // Home selected
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone_outlined),
+            label: "Contact",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: "Favorites",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category_outlined),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
